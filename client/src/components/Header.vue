7aaea7ff159b16120d6fc75c99174925a4baf24a
Kyle <kyleryan480@gmail.com>
2018-12-04 12:56:32 -0700

    navigating from header + styling

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,17 +1,48 @@
 <template>
   <v-toolbar fixed class="cyan" dark>
     <v-toolbar-title class="mr-4">
-      TabTracker
+      <span
+        class="home"
+        @click="navigateTo({name: 'root'})">
+        TabTracker
+      </span>
     </v-toolbar-title>
+
+    <!-- TODO: Implement Browse Button -->
+    <!-- <v-toolbar-items>
+      <v-btn flat>
+        Browse
+      </v-btn>
+    </v-toolbar-items> -->
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items>
+        <v-btn
+          flat
+          @click="navigateTo({name: 'register'})">
+          Sign Up
+        </v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  }
 }
 </script>
 
 <style scoped>
+.home {
+  cursor: pointer;
+}
 
+.home:hover {
+  color: #E9E
+}
 </style>
