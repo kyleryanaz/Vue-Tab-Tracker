d369bec9d9d04f5a297cc15c15dec3bb61f5ec0f
Kyle <kyleryan480@gmail.com>
2018-12-06 18:39:03 -0700

    song create fields required

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -4,31 +4,45 @@
       <panel title="Song Metadata">
         <v-text-field
           label="Title"
+          required
+          :rules="[required]"
           v-model="song.title"
         ></v-text-field>
 
         <v-text-field
           label="Artist"
+          required
+          :rules="[required]"
           v-model="song.artist"
         ></v-text-field>
 
         <v-text-field
           label="Genre"
+          required
+          :rules="[required]"
           v-model="song.genre"
         ></v-text-field>
 
         <v-text-field
           label="Album"
+          required
+          :rules="[required]"
           v-model="song.album"
         ></v-text-field>
 
         <v-text-field
-          label="Album Image Url"
+          label="Album I
+          mage Url"
+          required
+          :rules="[required]"
           v-model="song.albumImageUrl"
         ></v-text-field>
 
         <v-text-field
-          label="YouTube Id"
+          label="YouTube
+           Id"
+           required
+          :rules="[required]"
           v-model="song.youtubeId"
         ></v-text-field>
       </panel>
@@ -39,15 +53,23 @@
         <v-text-field
           label="Tab"
           multi-line
+          required
+          :rules="[required]"
           v-model="song.tab"
         ></v-text-field>
 
         <v-text-field
           label="Lyrics"
           multi-line
+          required
+          :rules="[required]"
           v-model="song.lyrics"
         ></v-text-field>
       </panel>
+
+      <div class="danger-alert" v-if="error">
+        {{error}}
+      </div>
 
       <v-btn
         dark
@@ -75,11 +97,21 @@ export default {
         youtubeId: null,
         lyrics: null,
         tab: null
-      }
+      },
+      error: null,
+      required: (value) => !!value || 'Required.'
     }
   },
   methods: {
     async create () {
+      this.error = null
+      const areAllFieldsFilledIn = Object
+        .keys(this.song)
+        .every(key => !!this.song[key])
+      if (!areAllFieldsFilledIn) {
+        this.error = 'Please fill in all the required fields'
+        return
+      }
       try {
         await SongsService.post(this.song)
         this.$router.push({
