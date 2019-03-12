615afbef210fc864f18cccb3e98d2840c4b445eb
Kyle <kyleryan480@gmail.com>
2019-03-11 18:07:03 -0700

    added edit view + put route

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -32,5 +32,19 @@ module.exports = {
         error: 'Song post error'
       })
     }
+  },
+  async put (req, res) {
+    try {
+      const song = await Song.update(req.body, {
+        where: {
+          id: req.params.songId
+        }
+      })
+      res.send(req.body)
+    } catch (err) {
+      res.status(500).send({
+        error: 'Song put error'
+      })
+    }
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -15,7 +15,11 @@ module.exports = (app) => {
 
   app.get('/songs/:songId',
     SongsController.show)
+    
+  app.put('/songs/:songId',
+    SongsController.put)
 
   app.post('/songs',
     SongsController.post)
+
 }
